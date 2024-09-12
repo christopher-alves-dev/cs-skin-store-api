b11f4048d9094fa01f5ed5f7fd6c07efa2d65b00
Christopher Alves <christopher.alves.dev@gmail.com>
2024-09-11 23:18:44 -0300

    habilita cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: "http://localhost:3001", // Endereço do frontend
+    methods: "GET",
+  });
+
   await app.listen(3000);
 }
 bootstrap();
